7572f7e8d54b146883ca8438bec4c4220c48eb31
Dnyaneshwar <kasarednyaneshwar866@gmail.com>
2022-11-03 15:21:33 +0530

    add new class

---
+++ src/com/example/MyArray.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MyArray
+{
+    public static void main(String[] args)
+    {
+        int[] a={ 10,20,30,40,50};
+        for(int num:a)
+        {
+            System.out.println("my array element is :"+num);
+        }
+    }
+}
