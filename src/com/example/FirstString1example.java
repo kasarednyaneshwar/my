6a8709d7178e3a7393d0b0d0bccdd71dfeb4672d
Dnyaneshwar <kasarednyaneshwar866@gmail.com>
2022-11-07 13:33:39 +0530

    add String1 Example

---
+++ src/com/example/FirstString1example.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class FirstString1example
+{
+    public static void main(String[] args)
+    {
+       // String s1="Kaare";
+      //  String s2="kaare";
+        String s1="Akshay";
+        String s2="Rahul";
+        if(s1==s2)
+        //if(s1.equals(s2))
+        {
+            System.out.println("Print same string name");
+        }
+        else{
+            System.out.println(" printed String name is not same");
+        }
+    }
+}
